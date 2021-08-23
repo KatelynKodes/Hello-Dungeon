c23f03a5e69c69e83d0c6abffd84df31224266f8
Katelyn <katelynwest225@gmail.com>
2021-08-23 13:01:09 -0500

    8/23/21 - Finishing up text adventure second encounter and fixing bugs

--- Game.cs
+++ Game.cs
@@ -17,7 +17,8 @@ namespace Command_Line_Game1
 
         public void Run()
         {
-            PrintAllEven(1, 10);
+            validInput = false;
+            gameover = false;
             Console.WriteLine("Welcome adventurer");
             Console.ReadKey();
             Console.WriteLine("You have been chosen to carry out a daring advanture.");
@@ -34,14 +35,8 @@ namespace Command_Line_Game1
             charname = Console.ReadLine();
             Console.WriteLine(charname + ". Brilliant.");
             Console.ReadKey();
-            Console.WriteLine("Now, what is your class?");
-            Console.ReadKey();
-            Console.WriteLine("[B] Barbarian");
-            Console.ReadKey();
-            Console.WriteLine("[W] Wizard");
-            Console.ReadKey();
-            Console.WriteLine("[H] Healer");
-            Console.ReadKey();
+            Console.WriteLine("Now, what is your class? \n [B] Barbarian \n [W] Wizard \n [H] Healer");
+            Console.WriteLine(">");
             char_class = Console.ReadLine();
             while (!validInput)
             {
@@ -93,6 +88,12 @@ namespace Command_Line_Game1
                     validInput = false;
                     if (!gameover)
                     {
+                        BingusEncounter();
+                        validInput = false;
+                        Console.WriteLine("You exit the dungeon, filled with glee that you managed to escape alive.");
+                        Console.WriteLine("You go back to the town, ready to go home and prepare yourself for the adventure that " +
+                            "awaits you tomorrow.");
+                        PlayAgain();
 
                     }
                     else
@@ -111,9 +112,8 @@ namespace Command_Line_Game1
             }
         }
 
-        /// <summary>
-        /// TEXT ADVENTURE METHODS
-        /// </summary>
+        //TEXT ADVENTURE METHODS
+
         //Encounter1
         public void BullfrogEncounter()
         {
@@ -160,6 +160,10 @@ namespace Command_Line_Game1
         //Encounter2
         public void ChestEncounter()
         {
+            bool checkedChest2 = false;
+            bool checkedChest4 = false;
+            bool keynotfound = true;
+
             Console.WriteLine("Nonetheless, you venture onward into dungeon, surely the monsters further down are much scarier than just a bullfrog half your height");
             Console.ReadKey();
             Console.WriteLine("As you travel even deeper into the dungeon you come across four chests");
@@ -169,20 +173,21 @@ namespace Command_Line_Game1
             for (int i = 0; i < numOfChests; i++)
             {
                 int chestsLeft = numOfChests;
-                Console.WriteLine("Which Chest will you choose?" + "\n [1] Chest 1 \n[2] Chest 2 \n [3] Chest 3 \n [4] Chest 4");
+                Console.WriteLine("Which Chest will you choose?" + "\n [1] Chest 1 \n [2] Chest 2 \n [3] Chest 3 \n [4] Chest 4");
                 Console.WriteLine("Number of attempts left: " + (chestsLeft = chestsLeft - i));
                 string ChestChosen = Console.ReadLine();
                 if (ChestChosen == "1" || ChestChosen.ToLower() == "chest 1")
                 {
                     Console.WriteLine("Upon examining the first chest, four animalistic legs sprout out from the bottom. The chest opens to reveal a large toothy mouth that could devour you whole.");
                     Console.WriteLine("The chest violently attacks you, killing you almost instantly.");
+                    gameover = true;
                     break;
                 }
                 else if (ChestChosen == "3" || ChestChosen.ToLower() == "chest 3")
                 {
                     Console.WriteLine("Upon examining the third chest, you unlock it to find a golden key matching the doors in front of you.");
                     Console.WriteLine("You grab the key and proceed forward.");
-                    gameover = true;
+                    keynotfound = false;
                     break;
                 }
                 else
@@ -191,15 +196,27 @@ namespace Command_Line_Game1
                     {
                         if (ChestChosen == "2" || ChestChosen.ToLower() == "chest 2")
                         {
-                            Console.WriteLine("Upon examining the second chest, you're dissapointed to find nothing at all.");
-                            Console.WriteLine("You don't get the key.");
-                            break;
+                            if (checkedChest2)
+                            {
+                                Console.WriteLine("You already checked chest 2");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Upon examining the second chest, you're dissapointed to find nothing at all.");
+                                Console.WriteLine("You don't get the key.");
+                            }
                         }
                         else
                         {
-                            Console.WriteLine("Upon examining the fourth chest, you're dissapointed to find nothing at all.");
-                            Console.WriteLine("You don't get the key.");
-                            break;
+                            if (checkedChest4)
+                            {
+                                Console.WriteLine("You already checked chest 4");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Upon examining the fourth chest, you're dissapointed to find nothing at all.");
+                                Console.WriteLine("You don't get the key.");
+                            }
                         }
                     }
                     else
@@ -208,15 +225,24 @@ namespace Command_Line_Game1
                     }
                 }
             }
+            if ()
+            {
+                
+            }
+            
         }
 
         //Encounter3
         public void GooseEncounter()
         {
             Console.WriteLine("Once you pass through the doors after the chests, you're met with yet another treacherous beast.");
+            Console.ReadKey();
             Console.WriteLine("A....Goose? Holding a knife.");
+            Console.ReadKey();
             Console.WriteLine("Such a monstrosity.");
+            Console.ReadKey();
             Console.WriteLine("Somehow the chests were better.");
+            Console.ReadKey();
             Console.WriteLine("Anyways, what do you do? \n [A] Attack Goose \n [B] Threaten Goose \n [C] Try to pass Goose");
             Console.WriteLine(">");
             UserInput = Console.ReadLine();
@@ -226,28 +252,43 @@ namespace Command_Line_Game1
                 {
                     validInput = true;
                     Console.WriteLine("You attack the goose!");
+                    Console.ReadKey();
                     Console.WriteLine("The goose, despite dying a painful death, seens to admire your violent tendencies.");
+                    Console.ReadKey();
                     Console.WriteLine("Truly an agent of chaos.");
+                    Console.ReadKey();
                     Console.WriteLine("The goose finally dies, no longer inflicting it's crimes of destruction onto anybody again.");
+                    Console.ReadKey();
                     Console.WriteLine("It also can't commit tax fraud anymore so that's good");
+                    Console.ReadKey();
                 }
                 else if (UserInput.ToLower() == "b" || UserInput.ToLower() == "threaten" || UserInput.ToLower() == "threaten goose")
                 {
                     validInput = true;
                     Console.WriteLine("You threaten the goose.");
+                    Console.ReadKey();
                     Console.WriteLine("The goose is impressed by your threat");
+                    Console.ReadKey();
                     Console.WriteLine("Yes, you seem like a violent yet reliable accomplaice.");
+                    Console.ReadKey();
                     Console.WriteLine("The goose gives you a gun.");
+                    Console.ReadKey();
                     Console.WriteLine("got gun.");
+                    Console.ReadKey();
                 }
                 else if (UserInput.ToLower() == "c" || UserInput.ToLower() == "pass" || UserInput.ToLower() == "pass goose")
                 {
                     validInput = true;
                     Console.WriteLine("You try to pass the goose peacefully.");
+                    Console.ReadKey();
                     Console.WriteLine("You are peaceful, therefore you are weak");
+                    Console.ReadKey();
                     Console.WriteLine("Mercy is for the weak.");
+                    Console.ReadKey();
                     Console.WriteLine("The goose starts running full speed towards you, knife in beak");
+                    Console.ReadKey();
                     Console.WriteLine("It kills you as you desprately try to reach for the door.");
+                    Console.ReadKey();
                     Console.WriteLine("Game Over");
                     gameover = true;
                 }
@@ -264,39 +305,55 @@ namespace Command_Line_Game1
         {
             int BingusPets = 0;
             int BingusAttacks = 0;
+            int Playerdamage = 0;
             bool bingusTurn = false;
 
-            while (BingusPets < 3 || BingusAttacks < 3)
+            Console.WriteLine("Entering the last room of the dungeon, you are met with a gigantic cat-like monstrosity");
+            Console.ReadKey();
+            Console.WriteLine("It's name being...");
+            Console.ReadKey();
+            Console.WriteLine("Bingus");
+            Console.ReadKey();
+            Console.WriteLine("(Seriously who comes up with these names?)");
+            Console.ReadKey();
+            Console.WriteLine("Whatever, names aside.");
+            Console.ReadKey();
+            while (!(BingusPets == 3 || BingusAttacks == 3 || Playerdamage == 3))
             {
                 if (bingusTurn)
                 {
                     Console.WriteLine("Bingus takes one of his paws and swipes at you. Attacking you");
+                    Playerdamage++;
                     bingusTurn = false;
                 }
                 else
                 {
                     Console.WriteLine("What do you do?");
-                    Console.WriteLine("[A] Attack Bingus \n [B] Pet Bingus");
+                    Console.WriteLine(" [A] Attack Bingus \n [B] Pet Bingus");
                     UserInput = Console.ReadLine();
                     if (UserInput.ToLower() == "a" || UserInput.ToLower() == "attack")
                     {
                         BingusAttacks++;
                         Console.WriteLine("You attack bingus, managing to do some damage.");
+                        Console.ReadKey();
                         Console.WriteLine("Bingus lets out a loud hiss at you.");
+                        Console.ReadKey();
                         bingusTurn = true;
                     }
                     else if (UserInput.ToLower() == "b" || UserInput.ToLower() == "pet")
                     {
                         BingusPets++;
                         Console.WriteLine("You reach up and manage to give Bingus a scratch on the head.");
+                        Console.ReadKey();
                         Console.WriteLine("A low rumble comes from the monsterous cat. It seems to like it.");
+                        Console.ReadKey();
                         bingusTurn = true;
 
                     }
                     else
                     {
                         Console.WriteLine("Invalid input.");
-                        UserInput = Console.ReadLine();
+                        bingusTurn = true;
                     }
                 }
             }
@@ -310,6 +367,12 @@ namespace Command_Line_Game1
             {
                 Console.WriteLine("You manage to tame the almighty Bingus.");
                 Console.WriteLine("Your are truly a beast to be reckoned with.");
+            }
+            else if (Playerdamage == 3)
+            {
+                Console.WriteLine("You die a terrible death at the paws of bingus.");
+                Console.WriteLine("Much like those who have fallen before you.");
+                gameover = true;
             }
         }
 
@@ -325,6 +388,7 @@ namespace Command_Line_Game1
                 if (UserInput.ToLower() == "y" || UserInput.ToLower() == "yes")
                 {
                     validInput = true;
+                    Console.Clear();
                     Run();
                 }
                 else if(UserInput.ToLower() == "n" || UserInput.ToLower() == "no")
@@ -335,12 +399,14 @@ namespace Command_Line_Game1
         }
 
 
-        /// <summary>
-        /// LETTUCE DEMONSTRATION GARBAGE
-        /// </summary>
+        //LODIS DEMONSTRATIONS
         
         //PrintAllEvenNums example
-        
+        /// <summary>
+        /// Prints all even numbers from num 1 to num 2
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
         void PrintAllEven(int num1, int num2)
         {
             for (int i = num1; i <= num2; i++)
@@ -386,8 +452,33 @@ namespace Command_Line_Game1
 
 
         //Option Choices
-        public void ChooseJob(string desc, string option1, string option2, string option3)
+        public void ChooseJob(string desc, string option1, string option2)
         {
+            int inputrecieved = 0;
+
+            Console.WriteLine(desc);
+            Console.WriteLine(option1 + "\n" + option2);
+            string input = Console.ReadLine();
+
+            while (!(inputrecieved == 1 || inputrecieved == 2))
+            {
+                if (input == "1" || input.ToLower() == option1.ToLower())
+                {
+                    inputrecieved = 1;
+                    Console.WriteLine(option1);
+                }
+                else if (input == "2" || input.ToLower() == option2.ToLower())
+                {
+                    inputrecieved = 2;
+                    Console.WriteLine(option2);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                    input = Console.ReadLine();
+                }
+            }
+
 
         }
 
